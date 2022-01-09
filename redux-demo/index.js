9a4076dba51442c92f094646a1550baad4b2d1c0
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-01-10 00:33:53 +0600

    REDUX-DEMO: Reducers

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -6,3 +6,24 @@ function buycake() {
     info: "First Redux App",
   };
 }
+
+// reducer(state, action) =>
+
+const initialState = {
+  numberOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+      break;
+
+    default:
+      return state;
+      break;
+  }
+};
